76f39a540ede15abdbbc813d2dae1bc092827064
Artur Ezhov <enproms@yandex.ru>
2023-04-26 23:04:54 +0500

    second commit

--- src/test/java/ru/netology/services/freelancServiceTest.java
+++ src/test/java/ru/netology/services/freelancServiceTest.java
@@ -1,7 +1,17 @@
 package ru.netology.services;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
 class freelancServiceTest {
+    @Test
+    public void testWhenFreelancerRestThreeMonts () {
+        freelancService service = new freelancService();
+
+        int actual = service.calculate( income: 10_000, expenses: 3_000, threshold: 20_000);
+        int expected = 3;
+
+        Assertions.assertEquals(expected, actual);
+    }
 
 }
